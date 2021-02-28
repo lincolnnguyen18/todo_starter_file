0f933068461182f86c89d5924e0a99b5783bf693
lincolnnguyen18 <letsmakeaworld@icloud.com>
2021-02-27 21:55:41 -0600

    Stopped scrollbar from pushing things left!

--- src/todo/ToDoController.js
+++ src/todo/ToDoController.js
@@ -14,21 +14,31 @@ export default class ToDoController {
         let appModel = this.model;
 
         // SETUP ALL THE EVENT HANDLERS SINCE THEY USE THE MODEL
-        document.getElementById("add-list-button").onmousedown = function() {
+        document.getElementById("add-list-button").onmouseup = function() {
             appModel.addNewList();
         }
-        document.getElementById("undo-button").onmousedown = function() {
+        document.getElementById("undo-button").onmouseup = function() {
             appModel.undo();
         }
-        document.getElementById("redo-button").onmousedown = function() {
+        document.getElementById("redo-button").onmouseup = function() {
             appModel.redo();
         }
-        document.getElementById("delete-list-button").onmousedown = function() {
+        document.getElementById("delete-list-button").onmouseup = function() {
             // appModel.removeCurrentList();
+            document.getElementById("modal_container").classList.add('show');
+            console.log("open clicked!");
         }
-        document.getElementById("add-item-button").onmousedown = function() {
+        document.getElementById("add-item-button").onmouseup = function() {
             appModel.addNewItemTransaction();
-        }  
+        }
+        document.getElementById("modal-cancel1").onmouseup = function() {
+            modal_container.classList.remove('show');
+            console.log("close clicked!");
+        }
+        document.getElementById("modal-cancel2").onmouseup = function() {
+            modal_container.classList.remove('show');
+            console.log("close clicked!");
+        }
     }
     
     // PROVIDES THE RESPONSE TO WHEN A USER CLICKS ON A LIST TO LOAD
